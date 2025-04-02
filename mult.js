21f316f1793c724803fd86f6596086627ca8dff6
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-02 10:12:39 +0500

    New mult.js

--- mult.js
+++ mult.js
@@ -1,143 +1,40 @@
-// animation_plugin.js
 (function () {
     'use strict';
 
-    var SourceTMDBAnimation = function (parent) {
-        this.network = new Lampa.Reguest();
+    // Функция для добавления кнопки в меню
+    function addMenuButton() {
+        console.log("Добавление кнопки в меню...");
 
-        this.main = function () {
-            var owner = this;
-            var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
-            var onComplete = arguments.length > 1 ? arguments[1] : undefined;
-            var onError = arguments.length > 2 ? arguments[2] : undefined;
-            var partsLimit = 6;
-
-            var tmdbBaseUrl = "https://api.themoviedb.org/3";  // Базовый URL для TMDB
-            var ratingFilter = 'certification_country=US&certification.lte=PG-13';
-
-            // Исправленная функция создания запроса
-            function createRequest(endpoint, title, callback) {
-                // Формируем корректный запрос к TMDB
-                let fullUrl = `${tmdbBaseUrl}${endpoint}?${ratingFilter}&language=ru-RU`;
-                console.log("Запрос к TMDB:", fullUrl);
-
-                owner.get(fullUrl, params, function (json) {
-                    json.title = Lampa.Lang.translate(title);
-
-                    // Формируем ссылку на изображение
-                    json.results.forEach(item => {
-                        if (item.poster_path) {
-                            item.poster_url = `http://192.168.0.247:9118/tmdb/img/t/p/w500/${item.poster_path}?uid=114576`;
-                        }
-                    });
-
-                    callback(json);
-                }, callback);
-            }
-
-            var partsData = [
-                function (callback) {
-                    createRequest(`/trending/movie/week`, 'Популярные мультфильмы', callback);
-                },
-                function (callback) {
-                    createRequest(`/movie/upcoming`, 'Новые мультфильмы', callback);
-                },
-                function (callback) {
-                    createRequest(`/trending/tv/week`, 'Популярные мультсериалы', callback);
-                },
-                function (callback) {
-                    createRequest(`/tv/on_the_air`, 'Новые мультсериалы', callback);
-                },
-                function (callback) {
-                    console.log("Загрузка 'Вы смотрели'...");
-                    let viewed = JSON.parse(Lampa.Storage.get("viewed", "{}"));
-                    let viewedIds = Object.keys(viewed);
-
-                    if (viewedIds.length === 0) {
-                        callback({ title: Lampa.Lang.translate('Вы смотрели'), results: [] });
-                        return;
-                    }
-
-                    let promises = viewedIds.map(id => {
-                        let item = viewed[id];
-                        let url = `${tmdbBaseUrl}/${item.type}/${id}`;
-
-                        return Lampa.TMDB.get(url, { language: 'ru-RU' }).then(data => {
-                            if (data.genres && data.genres.some(g => g.id === 16)) return null;
-                            return {
-                                title: data.title || data.name,
-                                poster_path: data.poster_path,
-                                id: data.id,
-                                release_date: data.release_date || data.first_air_date,
-                                vote_average: data.vote_average,
-                                type: item.type
-                            };
-                        }).catch(err => {
-                            console.error("Ошибка загрузки элемента 'Вы смотрели':", err);
-                            return null;
-                        });
-                    });
-
-                    Promise.all(promises).then(results => {
-                        let items = results.filter(item => item !== null).slice(0, 10);
-                        callback({ title: Lampa.Lang.translate('Вы смотрели'), results: items });
-                    }).catch(e => {
-                        console.error("Ошибка в 'Вы смотрели':", e);
-                        callback({ title: Lampa.Lang.translate('Вы смотрели'), results: [] });
-                    });
-                }
-            ];
-
-            function loadPart(partLoaded, partEmpty) {
-                Lampa.Api.partNext(partsData, partsLimit, partLoaded, partEmpty);
-            }
-
-            loadPart(onComplete, onError);
-            return loadPart;
-        };
-    };
-
-    // Функция для добавления кнопки в боковом меню
-    function add() {
-        console.log("Добавление источника ANIMATION...");
-
-        // Создаем источник анимации, но не добавляем в настройки
-        var animationSource = Object.assign({}, Lampa.Api.sources.tmdb, new SourceTMDBAnimation(Lampa.Api.sources.tmdb));
-        Lampa.Api.sources.animation = animationSource;
-
-        // Добавляем кнопку в боковое меню
-        try {
-            const menuItem = $('<li class="menu__item selector" data-action="mult">' +
+        // Создаем элемент меню с SVG иконкой
+        const menuItem = $(
+            '<li class="menu__item selector" data-action="animation">' +
+                '<div class="menu__icon">' +
+                    '<svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256">' +
+                        '<path d="M112,164a12,12,0,1,1-12-12A12,12,0,0,1,112,164Zm44-12a12,12,0,1,0,12,12A12,12,0,0,0,156,152Zm60,36a52,52,0,0,1-88,37.52A52,52,0,1,1,56.72,149.8,71.58,71.58,0,0,1,63,129C44,91.1,32.53,40.76,45.64,19.08A22,22,0,0,1,65.06,8c14.12,0,26,11.89,36.44,36.36,6.22,14.62,10.85,31.32,14,44.74a71.8,71.8,0,0,1,25,0c3.13-13.42,7.76-30.12,14-44.74C164.9,19.89,176.82,8,190.94,8a22,22,0,0,1,19.42,11.08C223.47,40.76,212,91.1,193,129a71.58,71.58,0,0,1,6.26,20.76A51.77,51.77,0,0,1,216,188ZM155.89,93.63a71.72,71.72,0,0,1,26.88,19.64A218.45,218.45,0,0,0,197.6,67.08c3.49-18.13,3.15-33-.93-39.72A6,6,0,0,0,190.94,24c-6.61,0-14.52,9.7-21.72,26.62C163.29,64.56,158.87,80.74,155.89,93.63ZM73.23,113.27a71.72,71.72,0,0,1,26.88-19.64c-3-12.89-7.4-29.07-13.33-43C79.58,33.7,71.67,24,65.06,24a6,6,0,0,0-5.73,3.36C55.25,34.1,54.91,49,58.4,67.08A218.45,218.45,0,0,0,73.23,113.27ZM200,188a35.87,35.87,0,0,0-13.34-28,8,8,0,0,1-2.92-5.45,56,56,0,0,0-111.48,0A8,8,0,0,1,69.34,160a36,36,0,1,0,47.28,54.21l-9.74-8.09a8,8,0,1,1,10.24-12.3L128,202.9l10.88-9.05a8,8,0,0,1,10.24,12.3l-9.74,8.09A36,36,0,0,0,200,188Z"></path>' +
+                    '</svg>' +
+                '</div>' +
                 '<div class="menu__text">Мультфильмы</div>' +
-                '</li>');
+            '</li>'
+        );
 
-            menuItem.on('hover:enter', function () {
-                console.log("Открытие страницы 'Мультфильмы'...");
-                Lampa.Activity.push({
-                    title: 'Мультфильмы',
-                    component: 'main',
-                    source: 'animation',
-                    page: 1
-                });
-                console.log("Активность 'Мультфильмы' запущена.");
-            });
-
-            // Вставляем кнопку в меню
+        // Добавляем кнопку в меню
+        try {
             $('.menu .menu__list').eq(0).append(menuItem);
-            console.log("Пункт меню 'Мультфильмы' успешно добавлен.");
+            console.log("Кнопка 'Мультфильмы' успешно добавлена.");
         } catch (e) {
-            console.error("Ошибка при добавлении пункта меню:", e);
+            console.error("Ошибка при добавлении кнопки в меню:", e);
         }
     }
 
-    // Проверяем готовность приложения и запускаем добавление
+    // Запуск плагина, когда приложение готово
     if (window.appready) {
-        add();
+        addMenuButton();
     } else {
         Lampa.Listener.follow('app', function (e) {
             if (e.type === 'ready') {
-                add();
+                addMenuButton();
             }
         });
     }
+
 })();
