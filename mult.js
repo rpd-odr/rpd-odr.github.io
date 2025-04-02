18e1e0ada0197c26ee2c96d9e97e3f253c5d8f5e
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-02 12:51:19 +0500

    Update mult.js

--- mult.js
+++ mult.js
@@ -1,47 +1,304 @@
+// Фильтр для исключения аниме (id жанра 16)
+function filterOutAnime(results) {
+    return results.filter(function (item) {
+        return !(item.genre_ids && item.genre_ids.includes(16));
+    });
+}
+
 (function () {
     'use strict';
 
-    // Функция для добавления кнопки в меню
-    function addAnimationButton() {
-        const menuItem = document.createElement("li");
-        menuItem.className = "menu__item selector";
-        menuItem.dataset.action = "animation";
-        menuItem.innerHTML = `
-            <div class="menu__ico">
-                <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" viewBox="0 0 256 256">
-                    <path d="M112,164a12,12,0,1,1-12-12A12,12,0,0,1,112,164Zm44-12a12,12,0,1,0,12,12A12,12,0,0,0,156,152Zm60,36a52,52,0,0,1-88,37.52A52,52,0,1,1,56.72,149.8,71.58,71.58,0,0,1,63,129C44,91.1,32.53,40.76,45.64,19.08A22,22,0,0,1,65.06,8c14.12,0,26,11.89,36.44,36.36,6.22,14.62,10.85,31.32,14,44.74a71.8,71.8,0,0,1,25,0c3.13-13.42,7.76-30.12,14-44.74C164.9,19.89,176.82,8,190.94,8a22,22,0,0,1,19.42,11.08C223.47,40.76,212,91.1,193,129a71.58,71.58,0,0,1,6.26,20.76A51.77,51.77,0,0,1,216,188ZM155.89,93.63a71.72,71.72,0,0,1,26.88,19.64A218.45,218.45,0,0,0,197.6,67.08c3.49-18.13,3.15-33-.93-39.72A6,6,0,0,0,190.94,24c-6.61,0-14.52,9.7-21.72,26.62C163.29,64.56,158.87,80.74,155.89,93.63ZM73.23,113.27a71.72,71.72,0,0,1,26.88-19.64c-3-12.89-7.4-29.07-13.33-43C79.58,33.7,71.67,24,65.06,24a6,6,0,0,0-5.73,3.36C55.25,34.1,54.91,49,58.4,67.08A218.45,218.45,0,0,0,73.23,113.27ZM200,188a35.87,35.87,0,0,0-13.34-28,8,8,0,0,1-2.92-5.45,56,56,0,0,0-111.48,0A8,8,0,0,1,69.34,160a36,36,0,1,0,47.28,54.21l-9.74-8.09a8,8,0,1,1,10.24-12.3L128,202.9l10.88-9.05a8,8,0,0,1,10.24,12.3l-9.74,8.09A36,36,0,0,0,200,188Z"></path>
-                </svg>
-            </div>
-            <div class="menu__text">Мультфильмы</div>
-        `;
+    function startPlugin() {
+        window.plugin_tmdb_mod_ready = true;
 
-        menuItem.addEventListener("click", () => {
-            Lampa.Activity.push({
-                url: "discover/movie?with_genres=16&sort_by=revenue.desc&with_original_language=ru|uk|en|be|zh|cn",
-                title: "Мультфильмы - TMDB",
-                component: "category_full",
-                source: "tmdb",
-                genres: 16,
-                sort: "now",
-                card_type: "true",
-                page: 1,
+        var Episode = function (data) {
+            var card = data.card || data;
+            var episode = data.next_episode_to_air || data.episode || {};
+            if (card.source == undefined) card.source = 'tmdb';
+            Lampa.Arrays.extend(card, {
+                title: card.name,
+                original_title: card.original_name,
+                release_date: card.first_air_date
             });
-        });
+            card.release_year = ((card.release_date || '0000') + '').slice(0, 4);
 
-        const menuList = document.querySelector(".menu .menu__list");
-        if (menuList && !document.querySelector('.menu__item[data-action="animation"]')) {
-            menuList.appendChild(menuItem);
+            function remove(elem) {
+                if (elem) elem.remove();
+            }
+
+            this.build = function () {
+                this.card = Lampa.Template.js('card_episode');
+                this.img_poster = this.card.querySelector('.card__img') || {};
+                this.img_episode = this.card.querySelector('.full-episode__img img') || {};
+                this.card.querySelector('.card__title').innerText = card.title;
+                this.card.querySelector('.full-episode__num').innerText = card.unwatched || '';
+                if (episode && episode.air_date) {
+                    this.card.querySelector('.full-episode__name').innerText = ('s' + (episode.season_number || '?') + 'e' + (episode.episode_number || '?') + '. ') + (episode.name || Lampa.Lang.translate('noname'));
+                    this.card.querySelector('.full-episode__date').innerText = episode.air_date ? Lampa.Utils.parseTime(episode.air_date).full : '----';
+                }
+
+                if (card.release_year == '0000') {
+                    remove(this.card.querySelector('.card__age'));
+                } else {
+                    this.card.querySelector('.card__age').innerText = card.release_year;
+                }
+
+                this.card.addEventListener('visible', this.visible.bind(this));
+            };
+
+            this.image = function () {
+                var _this = this;
+                this.img_poster.onload = function () {
+                };
+                this.img_poster.onerror = function () {
+                    _this.img_poster.src = './img/img_broken.svg';
+                };
+                this.img_episode.onload = function () {
+                    _this.card.querySelector('.full-episode__img').classList.add('full-episode__img--loaded');
+                };
+                this.img_episode.onerror = function () {
+                    _this.img_episode.src = './img/img_broken.svg';
+                };
+            };
+
+            this.create = function () {
+                var _this2 = this;
+                this.build();
+                this.card.addEventListener('hover:focus', function () {
+                    if (_this2.onFocus) _this2.onFocus(_this2.card, card);
+                });
+                this.card.addEventListener('hover:hover', function () {
+                    if (_this2.onHover) _this2.onHover(_this2.card, card);
+                });
+                this.card.addEventListener('hover:enter', function () {
+                    if (_this2.onEnter) _this2.onEnter(_this2.card, card);
+                });
+                this.image();
+            };
+
+            this.visible = function () {
+                if (card.poster_path) this.img_poster.src = Lampa.Api.img(card.poster_path);
+                else if (card.profile_path) this.img_poster.src = Lampa.Api.img(card.profile_path);
+                else if (card.poster) this.img_poster.src = card.poster;
+                else if (card.img) this.img_poster.src = card.img;
+                else this.img_poster.src = './img/img_broken.svg';
+                if (card.still_path) this.img_episode.src = Lampa.Api.img(episode.still_path, 'w300');
+                else if (card.backdrop_path) this.img_episode.src = Lampa.Api.img(card.backdrop_path, 'w300');
+                else if (episode.img) this.img_episode.src = episode.img;
+                else if (card.img) this.img_episode.src = card.img;
+                else this.img_episode.src = './img/img_broken.svg';
+                if (this.onVisible) this.onVisible(this.card, card);
+            };
+
+            this.destroy = function () {
+                this.img_poster.onerror = function () { };
+                this.img_poster.onload = function () { };
+                this.img_episode.onerror = function () { };
+                this.img_episode.onload = function () { };
+                this.img_poster.src = '';
+                this.img_episode.src = '';
+                remove(this.card);
+                this.card = null;
+                this.img_poster = null;
+                this.img_episode = null;
+            };
+
+            this.render = function (js) {
+                return js ? this.card : $(this.card);
+            };
+        }
+
+        var SourceTMDBkids = function (parent) {
+            this.network = new Lampa.Reguest();
+            this.discovery = false;
+
+            var ratingLimit = 'PG';
+
+            this.main = function () {
+                var owner = this;
+                var params = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : {};
+                var onComplete = arguments.length > 1 ? arguments[1] : undefined;
+                var onError = arguments.length > 2 ? arguments[2] : undefined;
+                var partsLimit = 6;
+
+                var sortOptions = [
+                    { key: 'vote_count.desc', title: 'Много голосов' },
+                    { key: 'first_air_date.desc', title: 'Новинки' },
+                    { key: 'popularity.desc', title: 'Популярные' },
+                    { key: 'revenue.desc', title: 'Интерес зрителей' }
+                ];
+
+                var kidsGenres = [
+                    { id: 16, title: 'мультфильмы' },
+                    { id: 10751, title: 'семейные' },
+                    { id: 14, title: 'фэнтези' },
+                    { id: 28, title: 'боевики' },
+                    { id: 35, title: 'комедии' },
+                    { id: 12, title: 'приключения' },
+                    { id: 878, title: 'фантастика' }
+                ];
+
+                var forKids = [
+                    { id: 101, title: 'Lego' },
+                    { id: 102, title: 'Том и джерри' },
+                    { id: 103, title: 'Микки маус' }
+                ];
+
+                var streamingServices = [
+                    { id: 49, title: 'HBO' },
+                    { id: 2552, title: 'Apple TV+' },
+                    { id: 453, title: 'Hulu' },
+                    { id: 1024, title: 'Amazon Prime' },
+                    { id: 213, title: 'Netflix' },
+                    { id: 3186, title: 'HBO Max' }
+                ];
+
+                var kidsStudios = [
+                    { id: 2, title: 'Disney' },
+                    { id: 3, title: 'Pixar' },
+                    { id: 521, title: 'DreamWorks Animation' }
+                ];
+
+                var ratingFilter = 'certification_country=US&certification.lte=' + ratingLimit;
+
+                function shuffleArray(array) {
+                    for (var i = array.length - 1; i > 0; i--) {
+                        var j = Math.floor(Math.random() * (i + 1));
+                        [array[i], array[j]] = [array[j], array[i]];
+                    }
+                }
+
+                function createRequest(endpoint, titleSuffix, callback) {
+                    var sort = sortOptions[Math.floor(Math.random() * sortOptions.length)];
+                    owner.get(endpoint + '&sort_by=' + sort.key + '&' + ratingFilter, params, function (json) {
+                        json.title = Lampa.Lang.translate(sort.title + ' ' + titleSuffix);
+                        callback(json);
+                    }, callback);
+                }
+
+                function getStreaming(serviceName, serviceId) {
+                    return function (callback) {
+                        createRequest(
+                            'discover/tv?with_networks=' + serviceId + '&with_genres=16,10751',
+                            'на ' + serviceName,
+                            callback
+                        );
+                    };
+                }
+
+                function getStudioMovies(studioName, studioId) {
+                    return function (callback) {
+                        var endpoint = 'discover/movie?with_companies=' + studioId;
+                        createRequest(endpoint, 'от студии ' + studioName, function (json) {
+                            callback(json);
+                        });
+                    };
+                }
+
+                function getMoviesWithoutGenre() {
+                    return function (callback) {
+                        createRequest(
+                            'discover/movie?' + ratingFilter,
+                            'Фильмы',
+                            callback
+                        );
+                    };
+                }
+
+                function searchByKeyword() {
+                    return function (callback) {
+                        forKids.forEach(function (keyword) {
+                            var endpoint = 'search/movie?query=' + encodeURIComponent(keyword.title);
+                            createRequest(endpoint, '(' + keyword.title + ')', callback);
+                        });
+                    };
+                }
+
+                var partsData = [
+                    function (callback) {
+                        createRequest('discover/movie?with_genres=10751,16&with_original_language=ru', 'русские мультфильмы', callback);
+                    },
+                    function (callback) {
+                        createRequest('discover/movie?with_genres=10751,16&with_original_language=uk|en|be|zh|cn', 'иностранные мультфильмы', callback);
+                    }
+                ];
+
+                streamingServices.forEach(function (service) {
+                    partsData.push(getStreaming(service.title, service.id));
+                });
+
+                kidsGenres.forEach(function (genre) {
+                    partsData.push(function (callback) {
+                        createRequest('discover/movie?with_genres=' + genre.id, '(' + genre.title + ')', callback);
+                    });
+                });
+
+                kidsStudios.forEach(function (studio) {
+                    partsData.push(getStudioMovies(studio.title, studio.id));
+                });
+
+                function randomWideFlag() {
+                    return Math.random() < 0.2;
+                }
+
+                function wrapWithWideFlag(requestFunc) {
+                    return function (callback) {
+                        requestFunc(function (json) {
+                            if (randomWideFlag()) {
+                                json.small = true;
+                                json.wide = true;
+
+                                if (Array.isArray(json.results)) {
+                                    json.results.forEach(function (card) {
+                                        card.promo = card.overview;
+                                        card.promo_title = card.title || card.name;
+                                    });
+                                }
+                            }
+                            callback(json);
+                        });
+                    };
+                }
+
+                partsData = partsData.map(wrapWithWideFlag);
+
+                partsData.push(searchByKeyword());
+                partsData.push(getMoviesWithoutGenre());
+
+                shuffleArray(partsData);
+
+                function loadPart(partLoaded, partEmpty) {
+                    Lampa.Api.partNext(partsData, partsLimit, partLoaded, partEmpty);
+                }
+
+                loadPart(onComplete, onError);
+                return loadPart;
+            };
+        };
+
+        function add() {
+            var tmdb_mod_kids = Object.assign({}, Lampa.Api.sources.tmdb, new SourceTMDBkids(Lampa.Api.sources.tmdb));
+
+            Lampa.Api.sources.tmdb_mod_kids = tmdb_mod_kids;
+
+            Object.defineProperty(Lampa.Api.sources, 'AVIAMOVIE KIDS', {
+                get: function () {
+                    return tmdb_mod_kids;
+                }
+            });
+
+            Lampa.Params.select('source', Object.assign({}, Lampa.Params.values['source'], {
+                'AVIAMOVIE KIDS': 'AVIAMOVIE KIDS'
+            }), 'tmdb');
+        }
+
+        if (window.appready) add();
+        else {
+            Lampa.Listener.follow('app', function (e) {
+                if (e.type == 'ready') { add(); }
+            });
         }
     }
 
-    // Проверка на готовность приложения
-    if (window.appready) {
-        addAnimationButton();
-    } else {
-        Lampa.Listener.follow("app", (e) => {
-            if (e.type === "ready") {
-                addAnimationButton();
-            }
-        });
-    }
+    if (!window.plugin_tmdb_mod_ready) startPlugin();
 })();
