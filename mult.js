54b801bd8922022a36c4e5f76fb8a41ff58c5134
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-08 15:35:24 +0500

    Update mult.js

--- mult.js
+++ mult.js
@@ -18,35 +18,20 @@
             )
             .on('hover:enter', function () {
                 Lampa.Activity.push({
-                    url: '',
+                    url: 'discover/movie',
                     title: 'Подборки мультфильмов',
-                    component: 'collections_main',
-                    page: 1
+                    component: 'category_full',
+                    source: 'tmdb',
+                    page: 1,
+                    filter: {
+                        with_genres: '16', // Только мультфильмы
+                        sort_by: 'popularity.desc'
+                    }
                 });
             });
 
         $('.menu .menu__list').append(button);
     }
-
-    Lampa.Component.add('collections_main', function () {
-        this.create = function () {
-            var categories = [
-                { title: 'Новинки', url: 'discover/movie', filter: { sort_by: 'primary_release_date.desc', 'primary_release_date.lte': new Date().toISOString().split('T')[0], 'vote_count.gte': '100' } },
-                { title: 'Популярное', url: 'discover/movie', filter: { sort_by: 'popularity.desc', 'vote_count.gte': '100' } },
-                { title: 'Ожидаемое', url: 'movie/upcoming', filter: { sort_by: 'popularity.desc' } }
-            ];
-            categories = Lampa.Arrays.shuffle(categories);
-            this.page = new Lampa.Main({
-                title: 'Подборки мультфильмов',
-                categories: categories.map(function (c) {
-                    return { title: c.title, url: c.url, filter: c.filter, source: 'tmdb', card_type: true };
-                })
-            });
-            this.page.render();
-            return this.page;
-        };
-        this.destroy = function () { if (this.page) this.page.destroy(); };
-    });
 
     function initPlugin() {
         if ($('style#collections-plugin').length === 0) {
