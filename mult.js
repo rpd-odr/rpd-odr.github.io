0722dbe2eaa369ae046fc2984c43c19600393fc0
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-08 15:49:18 +0500

    Update mult.js

--- mult.js
+++ mult.js
@@ -18,15 +18,24 @@
             )
             .on('hover:enter', function () {
                 const collections = [
-                    { title: 'Disney', url: 'discover/movie', filter: { with_companies: '2', with_genres: '16', sort_by: 'vote_average.desc' } }
+                    { title: 'Disney', url: 'discover/movie', filter: { with_companies: '2', with_genres: '16', sort_by: 'vote_average.desc' } },
+                    { title: 'Pixar', url: 'discover/movie', filter: { with_companies: '3', with_genres: '16', sort_by: 'vote_average.desc' } },
+                    { title: 'Cartoon Network', url: 'discover/tv', filter: { with_networks: '56', with_genres: '16', sort_by: 'vote_average.desc' } },
+                    { title: 'Новинки 2020-х', url: 'discover/movie', filter: { with_genres: '16', 'primary_release_date.gte': '2020-01-01', sort_by: 'primary_release_date.desc' } },
+                    { title: 'Классика 90-х', url: 'discover/movie', filter: { with_genres: '16', 'primary_release_date.lte': '1999-12-31', sort_by: 'vote_average.desc' } },
+                    { title: 'Высокий рейтинг', url: 'discover/movie', filter: { with_genres: '16', 'vote_average.gte': 7, sort_by: 'vote_average.desc' } },
+                    { title: 'Популярные сериалы', url: 'discover/tv', filter: { with_genres: '16', sort_by: 'popularity.desc' } },
+                    { title: 'Семейные мультфильмы', url: 'discover/movie', filter: { with_genres: '16,10751', sort_by: 'vote_average.desc' } }
                 ];
+
+                const shuffledCollections = Lampa.Arrays.shuffle(collections);
 
                 Lampa.Activity.push({
                     url: '',
                     title: 'Подборки мультфильмов',
-                    component: 'category_full',
+                    component: 'main', // Переключаемся на main
                     page: 1,
-                    categories: collections.map(c => ({
+                    categories: shuffledCollections.map(c => ({
                         title: c.title,
                         url: c.url,
                         filter: c.filter,
