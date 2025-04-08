02ecdecb54189bd90ef92c56af12564def1596a5
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-08 15:00:36 +0500

    Update mult.js

--- mult.js
+++ mult.js
@@ -1,15 +1,6 @@
 (function () {
     'use strict';
 
-    // Функция для перемешивания массива
-    function shuffle(array) {
-        for (let i = array.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [array[i], array[j]] = [array[j], array[i]];
-        }
-        return array;
-    }
-
     // Добавление кнопки в меню
     function addCollectionsButton() {
         if ($('.menu .menu__item-collections').length) return;
@@ -27,40 +18,86 @@
                     .text('Подборки')
             )
             .on('hover:enter', function () {
-                const collections = [
-                    { title: 'Disney', url: 'discover/movie', filter: { with_companies: '2', with_genres: '16' }, sort: 'vote_average.desc' },
-                    { title: 'Pixar', url: 'discover/movie', filter: { with_companies: '3', with_genres: '16' }, sort: 'vote_average.desc' },
-                    { title: 'Cartoon Network', url: 'discover/tv', filter: { with_networks: '56', with_genres: '16' }, sort: 'vote_average.desc' },
-                    { title: 'Новинки 2020-х', url: 'discover/movie', filter: { with_genres: '16', 'primary_release_date.gte': '2020-01-01' }, sort: 'primary_release_date.desc' },
-                    { title: 'Классика 90-х', url: 'discover/movie', filter: { with_genres: '16', 'primary_release_date.lte': '1999-12-31' }, sort: 'vote_average.desc' },
-                    { title: 'Высокий рейтинг', url: 'discover/movie', filter: { with_genres: '16', 'vote_average.gte': 7 }, sort: 'vote_average.desc' },
-                    { title: 'Популярные сериалы', url: 'discover/tv', filter: { with_genres: '16' }, sort: 'popularity.desc' },
-                    { title: 'Семейные мультфильмы', url: 'discover/movie', filter: { with_genres: '16,10751' }, sort: 'vote_average.desc' }
-                ];
-
-                const shuffledCollections = shuffle([...collections]);
-
                 Lampa.Activity.push({
                     url: '',
                     title: 'Подборки мультфильмов',
-                    component: 'category_full',
-                    page: 1,
-                    categories: shuffledCollections.map(collection => {
-                        console.log('Collection:', collection);
-                        return {
-                            title: collection.title,
-                            url: collection.url,
-                            filter: collection.filter,
-                            sort_by: collection.sort,
-                            source: 'tmdb',
-                            card_type: true
-                        };
-                    })
+                    component: 'collections_main',
+                    page: 1
                 });
             });
 
         $('.menu .menu__list').append(button);
     }
+
+    // Регистрация компонента
+    Lampa.Component.add('collections_main', function () {
+        this.create = function () {
+            const collections = [
+                {
+                    title: 'Disney',
+                    url: 'discover/movie',
+                    filter: { with_companies: '2', with_genres: '16', sort_by: 'vote_average.desc' }
+                },
+                {
+                    title: 'Pixar',
+                    url: 'discover/movie',
+                    filter: { with_companies: '3', with_genres: '16', sort_by: 'vote_average.desc' }
+                },
+                {
+                    title: 'Cartoon Network',
+                    url: 'discover/tv',
+                    filter: { with_networks: '56', with_genres: '16', sort_by: 'vote_average.desc' }
+                },
+                {
+                    title: 'Новинки 2020-х',
+                    url: 'discover/movie',
+                    filter: { with_genres: '16', 'primary_release_date.gte': '2020-01-01', sort_by: 'primary_release_date.desc' }
+                },
+                {
+                    title: 'Классика 90-х',
+                    url: 'discover/movie',
+                    filter: { with_genres: '16', 'primary_release_date.lte': '1999-12-31', sort_by: 'vote_average.desc' }
+                },
+                {
+                    title: 'Высокий рейтинг',
+                    url: 'discover/movie',
+                    filter: { with_genres: '16', 'vote_average.gte': 7, sort_by: 'vote_average.desc' }
+                },
+                {
+                    title: 'Популярные сериалы',
+                    url: 'discover/tv',
+                    filter: { with_genres: '16', sort_by: 'popularity.desc' }
+                },
+                {
+                    title: 'Семейные мультфильмы',
+                    url: 'discover/movie',
+                    filter: { with_genres: '16,10751', sort_by: 'vote_average.desc' }
+                }
+            ];
+
+            // Перемешиваем категории
+            const shuffledCollections = Lampa.Arrays.shuffle(collections);
+
+            // Создаём страницу
+            this.page = new Lampa.Main({
+                title: 'Подборки мультфильмов',
+                categories: shuffledCollections.map(c => ({
+                    title: c.title,
+                    url: c.url,
+                    filter: c.filter,
+                    source: 'tmdb',
+                    card_type: true
+                }))
+            });
+
+            this.page.render();
+            return this.page;
+        };
+
+        this.destroy = function () {
+            if (this.page) this.page.destroy();
+        };
+    });
 
     // Инициализация плагина
     function initPlugin() {
