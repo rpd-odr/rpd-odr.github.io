5ba335cafe2d6a7c887a5e81f84ed207adc21634
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-09 10:18:35 +0500

    Update kuv-git.js

--- kuv-git.js
+++ kuv-git.js
@@ -1,15 +1,14 @@
 (function () {
     'use strict';
 	
-    var timer = setInterval(function(){
-        if(typeof Lampa !== 'undefined'){
-            clearInterval(timer);
-  
-Lampa.Utils.putScriptAsync([
-	"https://rpd-odr.github.io/kuv-style.js",
-	"https://rpd-odr.github.io/mult.js",
-        "https://rpd-odr.github.io/studios.js"
-], function() {});
-        }
-    },200);
+Lampa.Listener.follow("app", function(e) {
+    if (e.type === "ready") {
+        // Ваш код инициализации здесь
+        Lampa.Utils.putScriptAsync([
+            "https://rpd-odr.github.io/mult.js",
+            "https://rpd-odr.github.io/studios.js",
+            "https://rpd-odr.github.io/kuv-style.js"
+        ], function() {});
+    }
+});
 })();
