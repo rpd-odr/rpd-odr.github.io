2902e1c9bb2accf5d42ac2b571dc578dd54e4d2e
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-09 09:05:16 +0500

    Create kuv-git.js

---
+++ kuv-git.js
@@ -0,0 +1,15 @@
+(function () {
+    'use strict';
+	
+    var timer = setInterval(function(){
+        if(typeof Lampa !== 'undefined'){
+            clearInterval(timer);
+  
+Lampa.Utils.putScriptAsync([
+"https://rpd-odr.github.io/mult.js",
+"https://rpd-odr.github.io/studios.js",
+"https://rpd-odr.github.io/kuv-style.js",
+], function() {});
+        }
+    },200);
+})();
