0c37041ec531224640ef22126a3224f182f4d7bc
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-09 09:29:41 +0500

    Update kuv-git.js

--- kuv-git.js
+++ kuv-git.js
@@ -6,9 +6,9 @@
             clearInterval(timer);
   
 Lampa.Utils.putScriptAsync([
-"https://rpd-odr.github.io/mult.js",
-"https://rpd-odr.github.io/studios.js",
-"https://rpd-odr.github.io/kuv-style.js"
+	"https://rpd-odr.github.io/kuv-style.js",
+        "https://rpd-odr.github.io/mult.js",
+        "https://rpd-odr.github.io/studios.js"
 ], function() {});
         }
     },200);
