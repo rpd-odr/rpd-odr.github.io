51d4895f01d0300b3d2350fee10831c365f47499
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-04-28 08:14:34 +0500

    Update kstst2.js

--- kstst2.js
+++ kstst2.js
@@ -4,11 +4,9 @@
     function addLogo(render, movie) {
         console.log('=== Adding logo ===');
         
-        // Находим постер
-        var $poster = $('.full-start-new__poster', render);
-        console.log('Poster found:', $poster.length > 0);
-        
-        if (!$poster.length) return;
+        // Проверяем, находимся ли мы в портретном режиме
+        var $portrait = $('.full-start-new__poster', render);
+        if (!$portrait.length) return;
 
         // Получаем URL для запроса изображений
         var url = Lampa.TMDB.api((movie.name ? 'tv' : 'movie') + '/' + movie.id + '/images?api_key=' + Lampa.TMDB.key() + '&language=' + Lampa.Storage.get('language'));
@@ -22,11 +20,14 @@
                 if (logoPath) {
                     console.log('Logo path:', logoPath);
 
+                    // Скрываем оригинальное название
+                    $('.full-start-new__title', render).hide();
+
                     // Удаляем старый логотип если есть
                     $('.logo-container').remove();
 
                     // Добавляем relative к постеру
-                    $poster.css('position', 'relative');
+                    $portrait.css('position', 'relative');
 
                     // Создаем контейнер для логотипа
                     var $container = $('<div>')
@@ -41,18 +42,19 @@
 
                     // Создаем изображение логотипа
                     var $logo = $('<img>')
-                        .attr('src', Lampa.TMDB.image('/t/p/w300' + logoPath.replace('.svg', '.png')))
+                        .attr('src', Lampa.TMDB.image('/t/p/w300' + logoPath))
                         .css({
-                            'max-width': '200px',
-                            'max-height': '100px',
-                            'object-fit': 'contain'
+                            'max-width': '12.5em',    // 200px -> 12.5em (при базовом размере 16px)
+                            'max-height': '6.25em',   // 100px -> 6.25em
+                            'object-fit': 'contain',
+                            'filter': 'drop-shadow(0px 0px 0.125em rgba(0,0,0,0.5))'  // 2px -> 0.125em
                         });
 
                     // Добавляем логотип в контейнер
                     $container.append($logo);
 
                     // Добавляем контейнер к постеру
-                    $poster.append($container);
+                    $portrait.append($container);
                     
                     console.log('=== Logo added ===');
                 }
