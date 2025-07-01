a5c61b78ecda77cb8448a252d3dd1eac7b602de5
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-07-01 09:16:15 +0500

    Update orig-poster.js

--- orig-poster.js
+++ orig-poster.js
@@ -3,10 +3,12 @@
     window.lampa_settings = window.lampa_settings || {};
     Lampa.Storage.set('metadata_language', 'ru'); // Названия и описания на русском
     Lampa.Listener.follow('tmdb', function(e) {
-        if (e.type === 'request') {
-            // Используем оригинальный язык для постеров
-            e.data.params.language = e.data.movie.original_language || 'en'; // Оригинальный язык или английский по умолчанию
-            e.data.params.metadata_language = 'ru'; // Названия и описания на русском
+        if (e.type === 'request' && e.data.movie) {
+            // Устанавливаем язык постеров на основе original_language
+            var originalLang = e.data.movie.original_language || 'en'; // Английский как запасной
+            e.data.params.language = originalLang; // Язык постеров
+            e.data.params.metadata_language = 'ru'; // Язык метаданных
+            console.log('Poster language set to: ' + originalLang); // Для отладки
         }
     });
 })();
