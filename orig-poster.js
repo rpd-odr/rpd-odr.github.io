4887a6f335deb830afe926b56a5bd1eb59f7e82e
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-07-01 09:39:06 +0500

    Update orig-poster.js

--- orig-poster.js
+++ orig-poster.js
@@ -1,14 +1,22 @@
-(function() {
-    'use strict';
-    window.lampa_settings = window.lampa_settings || {};
-    Lampa.Storage.set('metadata_language', 'ru'); // Названия и описания на русском
-    Lampa.Listener.follow('tmdb', function(e) {
-        if (e.type === 'request' && e.data.movie) {
-            // Устанавливаем язык постеров на основе original_language
-            var originalLang = e.data.movie.original_language || 'en'; // Английский как запасной
-            e.data.params.language = originalLang; // Язык постеров
-            e.data.params.metadata_language = 'ru'; // Язык метаданных
-            console.log('Poster language set to: ' + originalLang); // Для отладки
-        }
-    });
-})();
+(function(plugin) {
+    plugin.start = function() {
+        // Устанавливаем язык интерфейса и текста на русский
+        localStorage.setItem('language', 'ru');
+        localStorage.setItem('tmdb_lang', 'ru');
+
+        // Перехватываем запросы к TMDB API
+        plugin.apps.tmdb = {
+            request: function(url, callback, error) {
+                if (url.includes('api.themoviedb.org')) {
+                    // Оставляем русский для текста, но запрашиваем оригинальные постеры
+                    url = url.replace(/(language=)[^&]*/, '$1ru');
+                    if (!url.includes('include_image_language')) {
+                        url += '&include_image_language=null';
+                    }
+                }
+                plugin.request(url, callback, error);
+            }
+        };
+    };
+    return plugin;
+})(window.plugin || {});
