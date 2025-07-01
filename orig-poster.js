3aa55e1fc5885b6469b2ab8776f343b90650f94d
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-07-01 10:56:44 +0500

    Update orig-poster.js

--- orig-poster.js
+++ orig-poster.js
@@ -1,63 +1,42 @@
 (function() {
     'use strict';
 
-    if (window.__tmdbLangForcePlugin) return;
-    window.__tmdbLangForcePlugin = true;
+    // Проверка на повторную загрузку
+    if (window.__tmdbPosterLangPlugin) return;
+    window.__tmdbPosterLangPlugin = true;
 
-    function init() {
-        const IMAGE_REGEX = /\/tmdb\/img\/t\/p\/([^/]+)\/(.+\.(jpg|png))(\?|$)/;
-        const API_REGEX = /\/tmdb\/api\/3\/(movie|tv)\/\d+/;
-        
-        // Главный перехватчик
-        const intercept = () => {
-            const originalFetch = window.fetch;
+    // Ждем полной загрузки Lampa
+    function initPlugin() {
+        // Проверяем необходимые API
+        if (!window.Lampa || !window.Lampa.TMDB) {
+            console.error('[PosterLang] Lampa.TMDB not found');
+            return;
+        }
+
+        // Сохраняем оригинальный метод
+        const originalGetImage = Lampa.TMDB.prototype.getImage;
+
+        // Переопределяем метод получения изображений
+        Lampa.TMDB.prototype.getImage = function(path, size) {
+            // Получаем оригинальный URL
+            let url = originalGetImage.call(this, path, size);
             
-            window.fetch = async function(input, init) {
-                const url = typeof input === 'string' ? input : input.url;
-                let newUrl = url;
-                
-                // Обработка API запросов
-                if (API_REGEX.test(url)) {
-                    newUrl = modifyApiUrl(url);
-                }
-                // Обработка изображений
-                else if (IMAGE_REGEX.test(url)) {
-                    newUrl = modifyImageUrl(url);
-                }
-                
-                if (newUrl !== url) {
-                    console.log('[LangForce] Modified URL:', newUrl);
-                    if (typeof input !== 'string') {
-                        return originalFetch(new Request(newUrl, input), init);
-                    }
-                    return originalFetch(newUrl, init);
-                }
-                
-                return originalFetch(input, init);
-            };
-        };
-        
-        // Модификация URL API
-        const modifyApiUrl = (url) => {
-            return url.replace(/([?&])language=[^&]*/, '$1language=original')
-                     .replace(/([?&]include_image_language)=[^&]*/, '');
-        };
-        
-        // Модификация URL изображений
-        const modifyImageUrl = (url) => {
-            // Удаляем все существующие параметры языка
-            let cleanUrl = url.split('?')[0];
+            // Добавляем параметры языка для постеров
+            if (url.includes('image.tmdb.org')) {
+                url += (url.includes('?') ? '&' : '?') + 
+                      'include_image_language=original,en,null';
+            }
             
-            // Добавляем приоритетные языки
-            return cleanUrl + '?language=original,en,null&' + (url.split('?')[1] || '');
+            return url;
         };
-        
-        // Запускаем перехватчик
-        intercept();
-        console.log('[LangForce] Plugin activated');
+
+        console.log('[PosterLang] Plugin successfully activated');
     }
 
-    // Инициализация
-    if (window.Lampa?.ready) init();
-    else document.addEventListener('lampa_ready', init);
+    // Варианты инициализации
+    if (window.Lampa && window.Lampa.ready) {
+        initPlugin();
+    } else {
+        document.addEventListener('lampa_ready', initPlugin);
+    }
 })();
