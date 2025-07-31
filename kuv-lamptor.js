cbad1147b13d83e53f33a810029a6a9ceab47fff
rpd-odr <127396462+rpd-odr@users.noreply.github.com>
2025-07-31 15:41:32 +0500

    Update kuv-lamptor.js

--- kuv-lamptor.js
+++ kuv-lamptor.js
@@ -210,7 +210,7 @@
 `; var W = `.download-card {
   all: unset;
   display: block;
-  width: 100%;
+  width: 90%;
   height: auto;
   margin: 0;
   margin-top: 0.75em;
@@ -467,7 +467,7 @@ var u = class a {
 				for (let o of e.torrents) B(o), H(o), J(o);
 			r.isConnected = !0;
 			let t = r.getClient().url;
-			T("Connected to " + t), a.notifyFirstTime(Lampa.Lang.translate("background-worker.connection-success") + ": " + t)
+			T("Connected to " + t); //a.notifyFirstTime(Lampa.Lang.translate("background-worker.connection-success") + ": " + t)
 		} catch (e) {
 			T("Error:", e), r.isConnected = !1, a.errorCount++, a.errorCount > 10 && (clearInterval(a.subscription), T("Stopping background worker due to too many errors"), a.notifyFirstTime(Lampa.Lang.translate("background-worker.error-detected")))
 		}
